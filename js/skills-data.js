81fb6319c1de805012d81709e1064499d197163c
dargaCode <darga.code@gmail.com>
2016-08-25 20:26:11 -0700

    Add data for skills - Add constants file with skills information - Load constants scripts in index.html
    This will allow me to render skills via JS rather than
    hardcoding them all in html

---
+++ js/skills-data.js
@@ -0,0 +1,115 @@
+
+const CONSTANTS = {
+  CATEGORY_ORDER: [
+    "Language",
+    "Environment",
+    "Framework",
+    "Library",
+    "Database",
+    "Tool"
+  ],
+  SKILLS: [
+    {
+      "name": "Bootstrap",
+      "projectCount": 5,
+      "iconClass": "devicon-bootstrap-plain",
+      "type": "Library"
+    },
+    {
+      "name": "C",
+      "projectCount": 18,
+      "iconClass": "devicon-c-plain",
+      "type": "Language"
+    },
+    {
+      "name": "C#",
+      "projectCount": 5,
+      "iconClass": "devicon-csharp-plain",
+      "type": "Language"
+    },
+    {
+      "name": "CSS",
+      "projectCount": 34,
+      "iconClass": "devicon-css3-plain",
+      "type": "Language"
+    },
+    {
+      "name": "Embedded JavaScript",
+      "projectCount": 7,
+      "iconClass": "devicon-javascript-plain",
+      "type": "Language"
+    },
+    {
+      "name": "Express.js",
+      "projectCount": 7,
+      "iconClass": "devicon-javascript-plain",
+      "type": "Framework"
+    },
+    {
+      "name": "Git",
+      "projectCount": 53,
+      "iconClass": "devicon-git",
+      "type": "Tool"
+    },
+    {
+      "name": "Gulp",
+      "projectCount": 1,
+      "iconClass": "devicon-gulp-plain",
+      "type": "Tool"
+    },
+    {
+      "name": "Heroku",
+      "projectCount": 7,
+      "iconClass": "devicon-heroku-plain",
+      "type": "Tool"
+    },
+    {
+      "name": "HTML",
+      "projectCount": 34,
+      "iconClass": "devicon-html5-plain",
+      "type": "Language"
+    },
+    {
+      "name": "JavaScript",
+      "projectCount": 37,
+      "iconClass": "devicon-javascript-plain",
+      "type": "Language"
+    },
+    {
+      "name": "jQuery",
+      "projectCount": 1,
+      "iconClass": "devicon-jquery-plain",
+      "type": "Library"
+    },
+    {
+      "name": "Linux Shell",
+      "projectCount": 53,
+      "iconClass": "devicon-linux-plain",
+      "type": "Tool"
+    },
+    {
+      "name": "MongoDB",
+      "projectCount": 3,
+      "iconClass": "devicon-mongodb-plain",
+      "type": "Database"
+    },
+    {
+      "name": "MySQL",
+      "projectCount": 1,
+      "iconClass": "devicon-mysql-plain",
+      "type": "Database"
+    },
+    {
+      "name": "Node.js",
+      "projectCount": 7,
+      "iconClass": "devicon-nodejs-plain",
+      "type": "Environment"
+    },
+    {
+      "name": "NPM",
+      "projectCount": 7,
+      "iconClass": "devicon-javascript-plain",
+      "type": "Tool"
+    }
+  ],
+}
---
+++ js/skills.js
@@ -0,0 +1,8 @@
+
+'use strict' //enable 'let'
+
+// crappy fake Require for now
+const CATEGORY_ORDER = CONSTANTS.CATEGORY_ORDER;
+const SKILLS = CONSTANTS.SKILLS;
+console.log(CATEGORY_ORDER);
+console.log(SKILLS);
