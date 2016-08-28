c2870d8d1ef29e46169bfd7cd9bb3363c0872aee
dargaCode <darga.code@gmail.com>
2016-08-27 18:58:45 -0700

    Render skills via JS instead of HTML - Remove skill lists from HTML - Sort skills via JS - Add skills to DOM via js - Style skill names

--- js/skills.js
+++ js/skills.js
@@ -1,8 +1,69 @@
 
 'use strict' //enable 'let'
 
+// CONSTANTS
+
 // crappy fake Require for now
 const CATEGORY_ORDER = CONSTANTS.CATEGORY_ORDER;
 const SKILLS = CONSTANTS.SKILLS;
-console.log(CATEGORY_ORDER);
-console.log(SKILLS);
+
+const skillParent = document.querySelector('.skills .skill-list');
+
+// VARIABLES
+
+
+// FUNCTIONS
+
+function getSortedSkills(sortHelper) {
+  return SKILLS.sort(sortHelper);
+}
+
+function addSkillsToDom(skillsArray) {
+
+  for (let skill of skillsArray) {
+
+    const skillElement = document.createElement('li');
+
+    skillElement.innerHTML = `${skill.type} - <span class="skill-name">${skill.name}</span> <span class="project-count">(${skill.projectCount} projects)</span>`;
+
+    skillParent.appendChild(skillElement);
+  }
+}
+
+// HELPERS
+
+function sortByProjectNum(a, b) {
+  return b.projectCount - a.projectCount;
+}
+
+function sortByType(a, b) {
+  a.order = CATEGORY_ORDER.indexOf(a.type);
+  b.order = CATEGORY_ORDER.indexOf(b.type);
+
+  if (a.order < b.order) {
+    return -1;
+  }
+  else if (b.order < a.order) {
+    return 1;
+  }
+  /* same type, so sort by name instead (to preserve alphabetical order within types) */
+  else {
+    return b.projectCount - a.projectCount;
+
+    if (a.name < b.name) {
+      return -1;
+    }
+    else if (b.name < a.name) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+}
+
+// MAIN
+
+const sortedSkills = getSortedSkills(sortByType);
+
+addSkillsToDom(sortedSkills);
