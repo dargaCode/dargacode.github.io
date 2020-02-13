18850ebf8f9e383d2794ac8d777972ab4a13a6f8
dargaCode <darga.code@gmail.com>
2020-02-13 02:56:48 -0800

    Disambiguate homepage nav vs react nav

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function NavigationBar(): JSX.Element {
   return (
     <header>
-      <nav>
+      <nav className="react-nav">
         <ul>
           <li>
             <NavLink to="/home" activeClassName="activeNavSection">
