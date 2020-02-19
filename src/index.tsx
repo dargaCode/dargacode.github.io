d556d7a56a19d535e12c6c02c4cb780699803db9
dargaCode <darga.code@gmail.com>
2020-02-19 00:03:30 -0800

    Extract remaining sections to react components

--- src/index.tsx
+++ src/index.tsx
@@ -12,45 +12,35 @@ import "./css/font-awesome.min.scss";
 import "./css/style.scss";
 import NavigationBar from "./components/navigation/NavigationBar";
 import AboutSection from "./components/about/AboutSection";
+import SkillsSection from "./components/skills/SkillsSection";
+import ProjectsSection from "./components/projects/ProjectsSection";
+import ResumeSection from "./components/resume/ResumeSection";
+import ContactSection from "./components/contact/ContactSection";
 
 // require("./js/study-duration.js");
-require("./js/skills.js");
-require("./js/projects.js");
+// require("./js/skills.js");
+// require("./js/projects.js");
 
 function App(): JSX.Element {
   return (
     <Router>
       <div className="router-container">
+        <div id="nocontent">
+          {/* ignored by google */}
+          <h1 className="invisible-but-outline-readable">
+            dargaCode - Darga Darga Programmer Portfolio
+          </h1>
+        </div>
         <header>
           <NavigationBar />
         </header>
 
         <Switch>
           <Route path="/about" component={AboutSection} />
-
-          <Route path="/skills">
-            <div>
-              <h2>Skills</h2>
-            </div>
-          </Route>
-
-          <Route path="/projects">
-            <div>
-              <h2>Projects</h2>
-            </div>
-          </Route>
-
-          <Route path="/resume">
-            <div>
-              <h2>Resume</h2>
-            </div>
-          </Route>
-
-          <Route path="/contact">
-            <div>
-              <h2>Contact</h2>
-            </div>
-          </Route>
+          <Route path="/skills" component={SkillsSection} />
+          <Route path="/projects" component={ProjectsSection} />
+          <Route path="/resume" component={ResumeSection} />
+          <Route path="/contact" component={ContactSection} />
 
           <Redirect from="/" to="about" />
         </Switch>
