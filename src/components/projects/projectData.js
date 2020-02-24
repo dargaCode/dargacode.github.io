aaa6f5f48f70c6f32eb2cdf4cc1ea6518a1030ca
dargaCode <darga.code@gmail.com>
2020-02-24 14:51:23 -0800

    Move project thumbnails folder
    (deleting unneeded files)

--- src/components/projects/projectData.js
+++ src/components/projects/projectData.js
@@ -7,7 +7,7 @@ export const PROJECTS = [
     description:
       "Generate, process, and store 2,000 people and businesses, then search them in real time. Incorporates Trie, Set, and Dictionary data structures to make sure search results are displayed extremely quickly.",
     date: "2017-04-04",
-    imagePath: "../../img/project-thumbnails/phonebook-search.jpg",
+    imagePath: "./project-thumbnails/phonebook-search.jpg",
     githubUrl: "https://github.com/dargaCode/PhonebookSearch",
     livePageUrl: "https://dargacode.com/PhonebookSearch/"
   },
@@ -18,7 +18,7 @@ export const PROJECTS = [
     description:
       "Manipulate the DOM in real time to process and highlight inputted text. Handle many edgecases related to pasting in rich text formatting or raw HTML code. CSS provides the illusion that highlights are happening in the input box.",
     date: "2017-03-08",
-    imagePath: "../../img/project-thumbnails/palindrome-highlighter.jpg",
+    imagePath: "./project-thumbnails/palindrome-highlighter.jpg",
     githubUrl: "https://github.com/dargaCode/PalindromeHighlighter",
     livePageUrl: "https://dargacode.com/PalindromeHighlighter/"
   },
@@ -29,7 +29,7 @@ export const PROJECTS = [
     description:
       "Let users import a headshot to the canvas, position and scale it to align with an overlay image, and download the result as a new file. This project focuses on Object-Oriented design and custom event handling.",
     date: "2017-01-21",
-    imagePath: "../../img/project-thumbnails/darkest-timeline.jpg",
+    imagePath: "./project-thumbnails/darkest-timeline.jpg",
     githubUrl: "https://github.com/dargaCode/DarkestTimelineMe",
     livePageUrl: "https://darkesttimeline.me"
   },
@@ -40,7 +40,7 @@ export const PROJECTS = [
     description:
       "Given a long description of an apartment and a query string, generates a highlight snippet made from only relevant phrases. Uses an array of dictionary objects to map categories to relevant keywords.",
     date: "2016-09-16",
-    imagePath: "../../img/project-thumbnails/apartment-search.jpg",
+    imagePath: "./project-thumbnails/apartment-search.jpg",
     githubUrl: "https://github.com/dargaCode/ApartmentDescriptionHighlights",
     livePageUrl: null
   }
