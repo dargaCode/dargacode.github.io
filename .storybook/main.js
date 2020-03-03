8c5e173a4e0132eaf6a9971eb48b3ba40398d8f0
dargaCode <darga.code@gmail.com>
2020-03-03 02:24:18 -0800

    Add scss loader to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,6 +10,10 @@ module.exports = {
         }
       ]
     });
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"]
+    });
     config.resolve.extensions.push(".ts", ".tsx");
     return config;
   }
