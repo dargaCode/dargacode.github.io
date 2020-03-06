97a997af5aa538a6ea035cef3361d70fc65936c6
dargaCode <darga.code@gmail.com>
2020-03-06 08:12:41 -0800

    Add test and story for ProjectsSection

---
+++ src/components/projects/ProjectsSection.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ProjectsSection from "./ProjectsSection";
+
+export default {
+  title: "Projects/ProjectsSection",
+  component: ProjectsSection
+};
+
+export const Basic = () => <ProjectsSection />;
