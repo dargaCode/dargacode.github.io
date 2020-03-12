707e9677422abd822e1b01c9c4f89b1ba87b893a
dargaCode <darga.code@gmail.com>
2020-03-11 18:17:20 -0700

    Render selector options dynamically

--- src/components/skills/SkillSortSelector.tsx
+++ src/components/skills/SkillSortSelector.tsx
@@ -3,14 +3,18 @@ import PropTypes from "prop-types";
 import { SKILL_SORT_OPTIONS } from "./SkillConstants";
 
 export default function SkillSortSelector(): JSX.Element {
+  function getOptions(sortOptions: string[]): JSX.Element[] {
+    return sortOptions.map((sortOption: string) => {
+      return <option key={sortOption}>{sortOption}</option>;
+    });
+  }
+
   return (
     <form>
       <label htmlFor="skill-sort">
         Sort by:
-        <select id="skill-sort" defaultValue="Skill Type">
-          <option>Skill Name</option>
-          <option>Skill Type</option>
-          <option>Project Count</option>
+        <select id="skill-sort" defaultValue={SKILL_SORT_OPTIONS[0]}>
+          {getOptions(SKILL_SORT_OPTIONS)}
         </select>
       </label>
     </form>
