dbfed4ae871f143a5e50755321d01692f8bc3d9e
dargaCode <darga.code@gmail.com>
2020-04-08 01:35:30 -0700

    Add stub event handling
    Reverse order of skills on dropdown change

--- src/components/skills/SkillsSection.tsx
+++ src/components/skills/SkillsSection.tsx
@@ -1,27 +1,44 @@
 import React from "react";
+import cloneDeep from "clone-deep";
 import SkillCard from "./SkillCard";
 import SkillSortSelector from "./SkillSortSelector";
-import { SKILLS } from "./skillData";
+import { SKILLS, Skill } from "./skillData";
 import "../_general.scss";
 import "./_skills.scss";
 
-export default class AboutSection extends React.Component {
+interface State {
+  skills: Skill[];
+}
+
+export default class SkillsSection extends React.Component<{}, State> {
   constructor(props: {}) {
     super(props);
+
+    this.state = {
+      skills: cloneDeep(SKILLS)
+    };
   }
 
+  handleSort = (): void => {
+    const { skills } = this.state;
+
+    this.setState({ skills: skills.reverse() });
+  };
+
   render(): JSX.Element {
+    const { skills } = this.state;
+
     return (
       <section id="skills" className="content-section skills">
         <div className="container">
           <header>
             <h2>Skills</h2>
 
-            <SkillSortSelector onChange={() => null} />
+            <SkillSortSelector onChange={this.handleSort} />
           </header>
 
           <div className="skill-list">
-            {SKILLS.map(skill => (
+            {skills.map((skill: Skill) => (
               <SkillCard skill={skill} key={skill.name} />
             ))}
           </div>
