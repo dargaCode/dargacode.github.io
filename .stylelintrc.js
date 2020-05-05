8597c0fa8f4875927d7c9134a1ba23fbde0d30b4
dargaCode <darga.code@gmail.com>
2020-05-04 18:01:44 -0700

    Add stylelint rules to prevent color literals
    some of which will be disabled in a new constants file

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-sass-guidelines"],
+  rules: {
+    "color-no-hex": true,
+    "color-no-invalid-hex": true,
+    "color-named": "never",
+    "function-whitelist": ["calc", "url"] // no rgb colors
+  }
 };
