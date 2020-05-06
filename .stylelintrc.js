a9d55333b089d51c73b20121bde4524f249ee627
dargaCode <darga.code@gmail.com>
2020-05-06 01:36:26 -0700

    Add `format` to stylelint function whitelist

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,6 @@ module.exports = {
     "color-no-hex": true,
     "color-no-invalid-hex": true,
     "color-named": "never",
-    "function-whitelist": ["calc", "url"] // no rgb colors
+    "function-whitelist": ["calc", "url", "format"] // no rgb colors
   }
 };
