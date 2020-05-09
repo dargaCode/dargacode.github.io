07ba57aa74b4c4f2ac0aef3204f7770fa1fbc81c
dargaCode <darga.code@gmail.com>
2020-05-09 00:10:25 -0700

    Convert ContentSection styles to scss module

--- src/components/contact/ContactSection.tsx
+++ src/components/contact/ContactSection.tsx
@@ -1,29 +1,32 @@
 import React from "react";
 import "../../_general.scss";
-import "./_contact.scss";
+import styles from "./ContactSection.styles.scss";
 
 export default function AboutSection(): JSX.Element {
   return (
-    <section id="contact" className="content-section contact">
-      <div className="container">
+    <section id="contact" className={`content-section ${styles.contact}`}>
+      <div className={styles.container}>
         <h2>Contact</h2>
 
-        <a className="button" href="mailto:resume@dargaCode.com">
-          <i className="icon-envelope" />
+        <a className={styles.button} href="mailto:resume@dargaCode.com">
+          <i className={`icon-envelope ${styles.envelope}`} />
           <span>Email</span>
         </a>
 
-        <a className="button" href="https://www.linkedin.com/in/dargaCode">
+        <a
+          className={styles.button}
+          href="https://www.linkedin.com/in/dargaCode"
+        >
           <i className="fa fa-linkedin-square" />
           <span>LinkedIn</span>
         </a>
 
-        <a className="button" href="https://github.com/dargaCode">
+        <a className={styles.button} href="https://github.com/dargaCode">
           <i className="fa fa-github-square" />
           <span>GitHub</span>
         </a>
 
-        <a className="button" href="https://blog.dargacode.com">
+        <a className={styles.button} href="https://blog.dargacode.com">
           <i className="fa fa-rss-square" />
           <span>Study Blog</span>
         </a>
