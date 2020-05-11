2f72d0efc60b74415b42e9df4bde919b3816fe7b
dargaCode <darga.code@gmail.com>
2020-05-11 16:30:17 -0700

    Convert ProjectsSection styles to scss module

--- src/components/projects/ProjectsSection.tsx
+++ src/components/projects/ProjectsSection.tsx
@@ -3,17 +3,17 @@ import cloneDeep from "clone-deep";
 import ProjectCard from "./ProjectCard";
 import { PROJECTS } from "./projectsData";
 import "../../_general.scss";
-import "./_projects.scss";
+import styles from "./ProjectsSection.styles.scss";
 
 export default function AboutSection(): JSX.Element {
   const projects = cloneDeep(PROJECTS);
 
   return (
-    <section id="projects" className="content-section projects">
-      <div className="container">
+    <section id="projects" className={`content-section ${styles.projects}`}>
+      <div className={styles.container}>
         <h2>Projects</h2>
 
-        <div className="project-list">
+        <div className={styles.projectList}>
           {projects.map(project => (
             <ProjectCard project={project} key={project.name} />
           ))}
