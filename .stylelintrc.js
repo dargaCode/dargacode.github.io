26a29c603e5491b6d556299668d8355fd89f58da
dargaCode <darga.code@gmail.com>
2020-05-22 02:46:45 -0700

    Ignore @include and pseudo-classes in max-nesting-depth

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-sass-guidelines"],
   rules: {
+    "max-nesting-depth": [
+      1,
+      {
+        ignore: ["pseudo-classes", "blockless-at-rules"],
+        ignoreAtRules: ["include"]
+      }
+    ],
     "color-no-hex": true,
     "color-no-invalid-hex": true,
     "color-named": "never",
