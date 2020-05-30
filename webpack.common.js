3665c036bb887da847234e3618dc688b3e43f0c4
dargaCode <darga.code@gmail.com>
2020-05-30 03:25:27 -0700

    Clean up "loader" vs "use":
    loader for singular, use for multiple

--- webpack.common.js
+++ webpack.common.js
@@ -37,7 +37,7 @@ module.exports = {
       },
       {
         test: /\.module\.scss$/,
-        loader: [
+        use: [
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
@@ -56,11 +56,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: "file-loader"
+        loader: "file-loader"
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: "file-loader"
+        loader: "file-loader"
       }
     ]
   },
