d9b1028c1d602551948c080ae2277705c4cafa57
dargaCode <darga.code@gmail.com>
2020-05-30 03:26:14 -0700

    Use presets rather than rules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,19 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
-
 module.exports = {
   stories: ["../src/components/**/*.stories.tsx"],
   addons: [
-    "@storybook/preset-scss",
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        cssLoaderOptions: {
+          modules: {
+            localIdentName: "[name]__[local]--[hash:base64:5]"
+          },
+          localsConvention: "dashesOnly"
+        }
+      }
+    },
+    "@storybook/preset-typescript",
     "@storybook/addon-knobs/register",
     "@storybook/addon-actions/register"
-  ],
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("ts-loader")
-        }
-      ]
-    });
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-    config.plugins.push(new MiniCssExtractPlugin());
-
-    return config;
-  }
+  ]
 };
