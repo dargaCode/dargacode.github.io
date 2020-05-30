f1d6349e915b4b8ebbef4c033743db5f93e808ac
dargaCode <darga.code@gmail.com>
2020-05-29 19:47:07 -0700

    Add path to scss rule

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
   stories: ["../src/components/**/*.stories.tsx"],
@@ -18,10 +19,12 @@ module.exports = {
     });
     config.module.rules.push({
       test: /\.scss$/,
-      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../")
     });
     config.resolve.extensions.push(".ts", ".tsx");
     config.plugins.push(new MiniCssExtractPlugin());
+
     return config;
   }
 };
