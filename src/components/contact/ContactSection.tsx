a9c4c36a9f479cb557a4f00798a6becd44f3caac
dargaCode <darga.code@gmail.com>
2020-06-03 04:58:35 -0700

    Clean up contact section responsiveness
    - including adding wrapper div for buttons
    - there's some junk here to adjust since the envelope icon
    comes from a different source than the others, and doesn't quite
    match

--- src/components/contact/ContactSection.tsx
+++ src/components/contact/ContactSection.tsx
@@ -8,28 +8,30 @@ export default function AboutSection(): JSX.Element {
       <div className={styles.container}>
         <h2>Contact</h2>
 
-        <a className={styles.button} href="mailto:resume@dargaCode.com">
-          <i className={`icon-envelope ${styles.envelope}`} />
-          <span>Email</span>
-        </a>
+        <div className={styles.buttons}>
+          <a className={styles.button} href="mailto:resume@dargaCode.com">
+            <i className={`icon-envelope ${styles.envelope}`} />
+            <span>Email</span>
+          </a>
 
-        <a
-          className={styles.button}
-          href="https://www.linkedin.com/in/dargaCode"
-        >
-          <i className="fa fa-linkedin-square" />
-          <span>LinkedIn</span>
-        </a>
+          <a
+            className={styles.button}
+            href="https://www.linkedin.com/in/dargaCode"
+          >
+            <i className="fa fa-linkedin-square" />
+            <span>LinkedIn</span>
+          </a>
 
-        <a className={styles.button} href="https://github.com/dargaCode">
-          <i className="fa fa-github-square" />
-          <span>GitHub</span>
-        </a>
+          <a className={styles.button} href="https://github.com/dargaCode">
+            <i className="fa fa-github-square" />
+            <span>GitHub</span>
+          </a>
 
-        <a className={styles.button} href="https://blog.dargacode.com">
-          <i className="fa fa-rss-square" />
-          <span>Study Blog</span>
-        </a>
+          <a className={styles.button} href="https://blog.dargacode.com">
+            <i className="fa fa-rss-square" />
+            <span>Study Blog</span>
+          </a>
+        </div>
       </div>
     </section>
   );
