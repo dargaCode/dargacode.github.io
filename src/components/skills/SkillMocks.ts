7988c055d3c1065a8bcc8ae6565d7c52038cc49b
dargaCode <darga.code@gmail.com>
2020-06-26 10:42:41 -0700

    Add `SkillMocks`

---
+++ src/components/skills/SkillMocks.ts
@@ -0,0 +1,35 @@
+import { Skill } from "./skillsUtils";
+
+export const MOCK_SKILLS: Skill[] = [
+  {
+    name: "javascript",
+    displayName: "JavaScript",
+    iconClass: "devicon-javascript-plain",
+    repoCount: 3
+  },
+  {
+    name: "jest",
+    displayName: "Jest",
+    iconClass: "devicon-jest-plain",
+    repoCount: 1
+  },
+  {
+    name: "mongodb",
+    displayName: "MongoDB",
+    iconClass: "devicon-mongodb-plain",
+    repoCount: 2
+  },
+
+  {
+    name: "react",
+    displayName: "React.js",
+    iconClass: "devicon-react-plain",
+    repoCount: 2
+  },
+  {
+    name: "storybook",
+    displayName: "Storybook",
+    iconClass: "devicon-storybook-plain",
+    repoCount: 1
+  }
+];
