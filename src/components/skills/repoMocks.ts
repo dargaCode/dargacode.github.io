a808e511c26f80f2b73d522702855903299cba80
dargaCode <darga.code@gmail.com>
2020-06-25 17:36:19 -0700

    Extract mock constants to new `repoMocks` file

---
+++ src/components/skills/repoMocks.ts
@@ -0,0 +1,31 @@
+import moment from "moment";
+import { Repo, TopicStats } from "./repoUtils";
+
+export const MOCK_REPOS: Repo[] = [
+  {
+    name: "",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    pushed_at: "2016-08-26T21:40:06Z",
+    topics: ["react", "javascript", "jest"]
+  },
+  {
+    name: "",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    pushed_at: "2020-08-26T21:40:06Z",
+    topics: ["javascript", "storybook", "mongodb"]
+  },
+  {
+    name: "",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    pushed_at: "2018-08-26T21:40:06Z",
+    topics: ["react", "javascript", "mongodb"]
+  }
+];
+
+export const MOCK_TOPIC_STATS: TopicStats = {
+  react: { count: 2, updateTime: moment("2018-08-26T21:40:06Z") },
+  javascript: { count: 3, updateTime: moment("2020-08-26T21:40:06Z") },
+  jest: { count: 1, updateTime: moment("2016-08-26T21:40:06Z") },
+  storybook: { count: 1, updateTime: moment("2020-08-26T21:40:06Z") },
+  mongodb: { count: 2, updateTime: moment("2020-08-26T21:40:06Z") }
+};
--- src/components/skills/repoUtils.test.ts
+++ src/components/skills/repoUtils.test.ts
@@ -1,66 +1,37 @@
-import moment from "moment";
-import { Repo, TopicStats, aggregateRepoTopicStats } from "./repoUtils";
-
-const REPOS: Repo[] = [
-  {
-    name: "",
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    pushed_at: "2016-08-26T21:40:06Z",
-    topics: ["react", "javascript", "jest"]
-  },
-  {
-    name: "",
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    pushed_at: "2020-08-26T21:40:06Z",
-    topics: ["javascript", "storybook", "mongodb"]
-  },
-  {
-    name: "",
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    pushed_at: "2018-08-26T21:40:06Z",
-    topics: ["react", "javascript", "mongodb"]
-  }
-];
-
-const TOPIC_STATS: TopicStats = {
-  react: { count: 2, updateTime: moment("2018-08-26T21:40:06Z") },
-  javascript: { count: 3, updateTime: moment("2020-08-26T21:40:06Z") },
-  jest: { count: 1, updateTime: moment("2016-08-26T21:40:06Z") },
-  storybook: { count: 1, updateTime: moment("2020-08-26T21:40:06Z") },
-  mongodb: { count: 2, updateTime: moment("2020-08-26T21:40:06Z") }
-};
+import { aggregateRepoTopicStats } from "./repoUtils";
+import { MOCK_REPOS, MOCK_TOPIC_STATS } from "./repoMocks";
 
 describe("`repoUtils`", () => {
   describe("`aggregateRepoTopicStats`", () => {
     it("should return object with key for each topic", () => {
-      const topicStats = aggregateRepoTopicStats(REPOS);
+      const topicStats = aggregateRepoTopicStats(MOCK_REPOS);
 
       expect(Array.from(Object.keys(topicStats))).toEqual(
-        Object.keys(TOPIC_STATS)
+        Object.keys(MOCK_TOPIC_STATS)
       );
     });
     it("should count single-use topics", () => {
-      const topicStats = aggregateRepoTopicStats(REPOS);
+      const topicStats = aggregateRepoTopicStats(MOCK_REPOS);
 
-      expect(topicStats.jest.count).toBe(TOPIC_STATS.jest.count);
+      expect(topicStats.jest.count).toBe(MOCK_TOPIC_STATS.jest.count);
     });
     it("should count multi-use topics", () => {
-      const topicStats = aggregateRepoTopicStats(REPOS);
+      const topicStats = aggregateRepoTopicStats(MOCK_REPOS);
 
-      expect(topicStats.react.count).toBe(TOPIC_STATS.react.count);
+      expect(topicStats.react.count).toBe(MOCK_TOPIC_STATS.react.count);
     });
     it("should track most recent update for single-use topics", () => {
-      const topicStats = aggregateRepoTopicStats(REPOS);
+      const topicStats = aggregateRepoTopicStats(MOCK_REPOS);
 
       expect(topicStats.storybook.updateTime).toStrictEqual(
-        TOPIC_STATS.storybook.updateTime
+        MOCK_TOPIC_STATS.storybook.updateTime
       );
     });
     it("should track most recent update for multi-use topics", () => {
-      const topicStats = aggregateRepoTopicStats(REPOS);
+      const topicStats = aggregateRepoTopicStats(MOCK_REPOS);
 
       expect(topicStats.jest.updateTime).toStrictEqual(
-        TOPIC_STATS.jest.updateTime
+        MOCK_TOPIC_STATS.jest.updateTime
       );
     });
   });
