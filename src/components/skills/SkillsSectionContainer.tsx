0ebac6827cc09e8a582b8fff993b4c2af2e3b9b7
dargaCode <darga.code@gmail.com>
2020-06-27 04:56:09 -0700

    Merge raw skills with github topic stats from api

--- src/components/skills/SkillsSectionContainer.tsx
+++ src/components/skills/SkillsSectionContainer.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import moment from "moment";
-import { RepoTopicStats, aggregateRepoTopicStats } from "./repoUtils";
+import { aggregateRepoTopicStats } from "./repoUtils";
 import SkillsSection from "./SkillsSection";
-import { MOCK_SKILLS } from "./mockSkills";
+import { RAW_SKILLS } from "./dataRawSkills";
+import { Skill } from "./skillsUtils";
 
 interface State {
   loading: boolean;
   error: Error | null;
-  topicStats: RepoTopicStats | {};
+  skills: Skill[];
 }
+
+const GITHUB_REPOS_API_URL =
+  "https://api.github.com/users/dargacode/repos?per_page=100";
 
 export default class SkillsSectionContainer extends React.Component<{}, State> {
   constructor(props: {}) {
@@ -17,21 +20,27 @@ export default class SkillsSectionContainer extends React.Component<{}, State> {
     this.state = {
       loading: true,
       error: null,
-      topicStats: {}
+      skills: []
     };
   }
 
   componentDidMount(): void {
-    fetch("https://api.github.com/users/dargacode/repos?per_page=100", {
+    fetch(GITHUB_REPOS_API_URL, {
       // eslint-disable-next-line spellcheck/spell-checker
       headers: { Accept: "application/vnd.github.mercy-preview+json" } // enable topics beta from github api
     })
       .then(response => response.json())
       .then(
         result => {
+          // merge the raw skills with the api topic data
+          const topicStats = aggregateRepoTopicStats(result);
+          const skills: Skill[] = RAW_SKILLS.map(skill => {
+            return Object.assign(skill, topicStats[skill.name]);
+          });
+
           this.setState({
             loading: false,
-            topicStats: aggregateRepoTopicStats(result)
+            skills
           });
         },
         error => {
@@ -44,7 +53,7 @@ export default class SkillsSectionContainer extends React.Component<{}, State> {
   }
 
   render(): JSX.Element {
-    const { loading, error, topicStats } = this.state;
+    const { loading, error, skills } = this.state;
 
     if (loading) {
       return <div>Loading...</div>;
@@ -53,21 +62,6 @@ export default class SkillsSectionContainer extends React.Component<{}, State> {
       return <div>Error: {error.message}</div>;
     }
 
-    const topics: {
-      name: string;
-      repoCount: number;
-      recentRepoUpdateTime: moment.Moment;
-    }[] = [];
-
-    Object.entries(topicStats).forEach(([name, stats]) => {
-      topics.push({
-        name,
-        repoCount: stats.repoCount,
-        recentRepoUpdateTime: stats.recentRepoUpdateTime
-      });
-    });
-    topics.sort((a, b) => b.recentRepoUpdateTime.diff(a.recentRepoUpdateTime));
-
-    return <SkillsSection skills={MOCK_SKILLS} />;
+    return <SkillsSection skills={skills} />;
   }
 }
