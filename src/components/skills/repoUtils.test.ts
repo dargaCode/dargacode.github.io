3ec0bf5d46b9deb17b26344cd7839caccbc0fb80
dargaCode <darga.code@gmail.com>
2020-06-29 01:18:10 -0700

    Add `getTimeSinceCommit` tests

--- src/components/skills/repoUtils.test.ts
+++ src/components/skills/repoUtils.test.ts
@@ -1,4 +1,5 @@
-import { aggregateRepoTopicStats } from "./repoUtils";
+import moment from "moment";
+import { aggregateRepoTopicStats, getTimeSinceCommit } from "./repoUtils";
 import { MOCK_REPOS, MOCK_REPO_TOPIC_STATS } from "./mockRepos";
 
 describe("`repoUtils`", () => {
@@ -37,6 +38,66 @@ describe("`repoUtils`", () => {
       expect(topicStats.jest.lastCommitTime).toStrictEqual(
         MOCK_REPO_TOPIC_STATS.jest.lastCommitTime
       );
+    });
+  });
+
+  describe("`getTimeSinceCommit`", () => {
+    describe("when time is less than a minute ago", () => {
+      it("should replace text with `1 minute ago`", () => {
+        const thirtySecondsAgo = moment().subtract(30, "seconds");
+
+        expect(getTimeSinceCommit(thirtySecondsAgo)).toBe("1 minute ago");
+      });
+    });
+    describe("when time is a minute ago", () => {
+      it("should replace text with `1 minute ago`", () => {
+        const oneMinuteAgo = moment().subtract(1, "minute");
+
+        expect(getTimeSinceCommit(oneMinuteAgo)).toBe("1 minute ago");
+      });
+    });
+    describe("when time is an hour ago", () => {
+      it("should replace text with `1 hour ago`", () => {
+        const oneHourAgo = moment().subtract(1, "hour");
+
+        expect(getTimeSinceCommit(oneHourAgo)).toBe("1 hour ago");
+      });
+    });
+    describe("when time is a day ago", () => {
+      it("should replace text with `1 day ago`", () => {
+        const oneDayAgo = moment().subtract(1, "day");
+
+        expect(getTimeSinceCommit(oneDayAgo)).toBe("1 day ago");
+      });
+    });
+    describe("when time is a month ago", () => {
+      it("should replace text with `1 month ago`", () => {
+        const oneMonthAgo = moment().subtract(1, "month");
+
+        expect(getTimeSinceCommit(oneMonthAgo)).toBe("1 month ago");
+      });
+    });
+    describe("when time is a year ago", () => {
+      it("should replace text with `1 year ago`", () => {
+        const oneYearAgo = moment().subtract(1, "year");
+
+        expect(getTimeSinceCommit(oneYearAgo)).toBe("1 year ago");
+      });
+    });
+    describe("when time is any other duration ago", () => {
+      it("should use the default `moment.fromNow` text", () => {
+        const thirtyMinutesAgo = moment().subtract(30, "minute");
+        const fiveHoursAgo = moment().subtract(5, "hour");
+        const twelveDaysAgo = moment().subtract(12, "day");
+        const sixMonthsAgo = moment().subtract(6, "month");
+        const eightYearsAgo = moment().subtract(8, "year");
+
+        expect(getTimeSinceCommit(thirtyMinutesAgo)).toBe("30 minutes ago");
+        expect(getTimeSinceCommit(fiveHoursAgo)).toBe("5 hours ago");
+        expect(getTimeSinceCommit(twelveDaysAgo)).toBe("12 days ago");
+        expect(getTimeSinceCommit(sixMonthsAgo)).toBe("6 months ago");
+        expect(getTimeSinceCommit(eightYearsAgo)).toBe("8 years ago");
+      });
     });
   });
 });
