63522716e62856f508ba9acd76146186acde23bf
dargaCode <darga.code@gmail.com>
2020-06-29 17:18:13 -0700

    Clean up comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,20 @@ module.exports = {
     "airbnb",
     "prettier",
     "prettier/react",
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:react/recommended",
     "plugin:css-modules/recommended",
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking", // slower but more powerful
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "prettier/@typescript-eslint", // disable eslint rules which conflict with prettier
+    "plugin:prettier/recommended" // enables eslint-plugin-prettier and eslint-config-prettier. this will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   plugins: ["react", "css-modules", "prettier", "spellcheck"],
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    ecmaVersion: 2018,
+    sourceType: "module", // allows for the use of imports
     ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
+      jsx: true
     },
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"]
@@ -72,7 +72,7 @@ module.exports = {
       }
     },
     react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: "detect"
     }
   }
 };
