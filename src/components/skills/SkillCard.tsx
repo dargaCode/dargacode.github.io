a0e8762b1a25a4a0de7b6285ae266b13d85904f7
dargaCode <darga.code@gmail.com>
2020-07-10 09:53:50 -0700

    Derive `iconClass` from skill `name`

--- src/components/skills/SkillCard.tsx
+++ src/components/skills/SkillCard.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../../config/_general.scss";
 import reactMomentPropTypes from "react-moment-proptypes";
-import styles from "./SkillCard.module.scss";
+import { startCase } from "lodash";
 import { Skill, SKILL_URL_PREFIX } from "./skillsUtils";
+import styles from "./SkillCard.module.scss";
 // eslint-disable-next-line css-modules/no-unused-class
 import icons from "../../fonts/icons.module.scss";
 
 export default function SkillCard(props: { skill: Skill }): JSX.Element {
   const { skill } = props;
-  const { name, displayName, iconClass, repoCount, timeSinceCommit } = skill;
+  const { name, displayName, repoCount, timeSinceCommit } = skill;
+  const iconClass = `icon${startCase(skill.name)}`;
 
   return (
     <a href={SKILL_URL_PREFIX + name}>
