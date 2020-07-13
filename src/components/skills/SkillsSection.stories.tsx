06577315b2a1bf4e690b92a54b1fd8b1025d2423
dargaCode <darga.code@gmail.com>
2020-07-13 06:17:40 -0700

    Add `Loading` knob to `SkillsSection` story

--- src/components/skills/SkillsSection.stories.tsx
+++ src/components/skills/SkillsSection.stories.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
 import SkillsSection from "./SkillsSection";
 import { MOCK_SKILLS } from "./mockSkills";
 
 export default {
   title: "Skills/SkillsSection",
-  component: SkillsSection
+  component: SkillsSection,
+  decorators: [withKnobs]
 };
 
-export const Basic = (): JSX.Element => <SkillsSection skills={MOCK_SKILLS} />;
+export const Basic = (): JSX.Element => (
+  <SkillsSection skills={MOCK_SKILLS} loading={boolean("Loading", false)} />
+);
