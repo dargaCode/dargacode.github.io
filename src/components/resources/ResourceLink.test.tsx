63e61ba6a19185ae5fef080c6cef4286481b63dd
dargaCode <darga.code@gmail.com>
2020-07-14 15:07:39 -0700

    Update test nesting

--- src/components/resources/ResourceLink.test.tsx
+++ src/components/resources/ResourceLink.test.tsx
@@ -9,26 +9,30 @@ import styles from "./ResourceLink.module.scss";
 const RESOURCE = MOCK_RESOURCES[0];
 
 describe("`ResourceLink`", () => {
-  describe("render", () => {
-    it("`should link to the resource's `href`", () => {
-      const wrapper = shallow(<ResourceLink resource={RESOURCE} />);
-      const { href } = wrapper.find(classSelector(styles.resourceLink)).props();
+  describe("props", () => {
+    describe("`resource`", () => {
+      it("`should link to the resource's `href`", () => {
+        const wrapper = shallow(<ResourceLink resource={RESOURCE} />);
+        const { href } = wrapper
+          .find(classSelector(styles.resourceLink))
+          .props();
 
-      expect(href).toBe(RESOURCE.href);
-    });
+        expect(href).toBe(RESOURCE.href);
+      });
 
-    it("`should render the resource's `displayName`", () => {
-      const wrapper = shallow(<ResourceLink resource={RESOURCE} />);
-      const nameText = wrapper.find(classSelector(styles.name)).text();
+      it("`should render the resource's `displayName`", () => {
+        const wrapper = shallow(<ResourceLink resource={RESOURCE} />);
+        const nameText = wrapper.find(classSelector(styles.name)).text();
 
-      expect(nameText).toBe(RESOURCE.displayName);
-    });
+        expect(nameText).toBe(RESOURCE.displayName);
+      });
 
-    it("`should render an icon with the resource's `iconClass`", () => {
-      const wrapper = shallow(<ResourceLink resource={RESOURCE} />);
-      const { className } = wrapper.find("i").props();
+      it("`should render an icon with the resource's `iconClass`", () => {
+        const wrapper = shallow(<ResourceLink resource={RESOURCE} />);
+        const { className } = wrapper.find("i").props();
 
-      expect(className).toBe(RESOURCE.iconClass);
+        expect(className).toBe(RESOURCE.iconClass);
+      });
     });
   });
 });
