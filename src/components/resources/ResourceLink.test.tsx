d53ff7a9aae0d36364d1fb5e9c4c4d35b12380ec
dargaCode <darga.code@gmail.com>
2020-07-14 14:07:38 -0700

    Update test

--- src/components/resources/ResourceLink.test.tsx
+++ src/components/resources/ResourceLink.test.tsx
@@ -12,9 +12,9 @@ describe("`ResourceLink`", () => {
   describe("render", () => {
     it("`should link to the resource's `href`", () => {
       const wrapper = shallow(<ResourceLink resource={RESOURCE} />);
-      const nameText = wrapper.find(classSelector(styles.link)).text();
+      const { href } = wrapper.find(classSelector(styles.resourceLink)).props();
 
-      expect(nameText).toBe(RESOURCE.displayName);
+      expect(href).toBe(RESOURCE.href);
     });
 
     it("`should render the resource's `displayName`", () => {
