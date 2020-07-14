3dc61cd75dcf2bf9284ba969d13db15b73e77c53
dargaCode <darga.code@gmail.com>
2020-07-14 08:05:49 -0700

    Add `MockResources`

---
+++ src/components/resources/MockResources.ts
@@ -0,0 +1,9 @@
+import { Resource } from "./ResourcesUtils";
+
+export const MOCK_RESOURCES: Resource[] = [
+  {
+    displayName: "Google",
+    href: "https://google.com",
+    iconClass: "iconGlobe"
+  }
+];
