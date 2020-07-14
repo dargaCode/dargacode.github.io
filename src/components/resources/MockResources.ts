ecbfcea37adc41f794c756bde61b0439a5cfb6ca
dargaCode <darga.code@gmail.com>
2020-07-14 15:49:57 -0700

    Update `MockResources` to have more entries

--- src/components/resources/MockResources.ts
+++ src/components/resources/MockResources.ts
@@ -5,5 +5,20 @@ export const MOCK_RESOURCES: Resource[] = [
     displayName: "Google Dotcom",
     href: "https://google.com",
     iconClass: "iconGlobe"
+  },
+  {
+    displayName: "Apple Dotcom",
+    href: "https://apple.com",
+    iconClass: "iconGlobe"
+  },
+  {
+    displayName: "Movies Dotcom",
+    href: "https://movies.com",
+    iconClass: "iconGlobe"
+  },
+  {
+    displayName: "Weather Dotcom",
+    href: "https://weather.com",
+    iconClass: "iconGlobe"
   }
 ];
