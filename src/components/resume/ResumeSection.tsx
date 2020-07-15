e42ec686a5d92e8161a604698397c9cbc5d78ec8
dargaCode <darga.code@gmail.com>
2020-07-14 23:15:57 -0700

    Replace `ContactSection` content with `ResourcesList`

--- src/components/resume/ResumeSection.tsx
+++ src/components/resume/ResumeSection.tsx
@@ -1,35 +1,18 @@
 import React from "react";
 import "../../config/_general.scss";
 import styles from "./ResumeSection.module.scss";
-// eslint-disable-next-line css-modules/no-unused-class
-import icons from "../../fonts/icons.module.scss";
-import pdfResume from "../../resumes/Darga_SWE_Resume.pdf";
-import docxResume from "../../resumes/Darga_SWE_Resume.docx";
+import ResourcesList from "../resources/ResourcesList";
+import { RESUME_RESOURCES } from "../resources/ResourcesData";
 
 export default function AboutSection(): JSX.Element {
   return (
     <section className={styles.contentSection}>
       <div className={styles.overlay}>
-        <div className={styles.container}>
+        {/* container style is not overwritten in `ResumeSection` module */}
+        <div className="container">
           <h2>Resume</h2>
 
-          <div className={styles.buttons}>
-            <a className={styles.button} href={pdfResume}>
-              <i className={icons.iconPdf} />
-              <span>
-                PDF <br />
-                Resume
-              </span>
-            </a>
-
-            <a className={styles.button} href={docxResume}>
-              <i className={icons.iconDocx} />
-              <span>
-                Simple <br />
-                Resume
-              </span>
-            </a>
-          </div>
+          <ResourcesList resources={RESUME_RESOURCES} />
         </div>
       </div>
     </section>
