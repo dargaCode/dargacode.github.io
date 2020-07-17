b7782ed3c1bd6c2d51c73f4429edc1e503dc4152
dargaCode <darga.code@gmail.com>
2020-07-17 05:17:05 -0700

    Add stories for `ContactSection` and `ResumeSection`

---
+++ src/components/contact/ContactSection.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ContactSection from "./ContactSection";
+
+export default {
+  title: "Contact/ContactSection",
+  component: ContactSection
+};
+
+export const Basic = (): JSX.Element => <ContactSection />;
---
+++ src/components/resume/ResumeSection.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ResumeSection from "./ResumeSection";
+
+export default {
+  title: "Resume/ResumeSection",
+  component: ResumeSection
+};
+
+export const Basic = (): JSX.Element => <ResumeSection />;
