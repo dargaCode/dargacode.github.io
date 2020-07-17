f7c54322778fe496fedcd347dac029d2f200aa68
dargaCode <darga.code@gmail.com>
2020-07-17 08:55:03 -0700

    Remove stories for now

--- src/components/contact/ContactSection.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ContactSection from "./ContactSection";
-
-export default {
-  title: "Contact/ContactSection",
-  component: ContactSection
-};
-
-export const Basic = (): JSX.Element => <ContactSection />;
--- src/components/resume/ResumeSection.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ResumeSection from "./ResumeSection";
-
-export default {
-  title: "Resume/ResumeSection",
-  component: ResumeSection
-};
-
-export const Basic = (): JSX.Element => <ResumeSection />;
