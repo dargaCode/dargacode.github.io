e0794b48cc9f56c4b8bb8181d9192f96988c71bb
dargaCode <darga.code@gmail.com>
2020-07-17 05:14:36 -0700

    Mock `.pdf` and `.docx` with `mockDocument.js`
    which resturns an empty string

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(jpg|png|svg)$": "<rootDir>/.jest/__mocks__/mockImage.js",
     "\\.(ttf|woff|woff2|pdf|docx)$": "<rootDir>/.jest/__mocks__/mockFile.js",
+    "\\.(pdf|docx)$": "<rootDir>/.jest/__mocks__/mockDocument.js",
     "^.+\\.(css|scss)$": "identity-obj-proxy"
   },
   transform: {
