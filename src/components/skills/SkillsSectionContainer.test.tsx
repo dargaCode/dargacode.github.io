d148d00e04ffc4b6a1f84fd0729210abafba1e75
dargaCode <darga.code@gmail.com>
2020-07-18 23:02:28 -0700

    Update test to reflect new fetch args

--- src/components/skills/SkillsSectionContainer.test.tsx
+++ src/components/skills/SkillsSectionContainer.test.tsx
@@ -5,7 +5,7 @@ import { MOCK_RAW_SKILLS, MOCK_SKILLS } from "./mockSkills";
 import { MOCK_REPOS } from "./mockRepos";
 import SkillsSectionContainer, {
   GITHUB_REPOS_API_URL,
-  GITHUB_REPOS_REQUEST_HEADERS
+  GITHUB_REPOS_FETCH_OPTIONS
 } from "./SkillsSectionContainer";
 
 describe("`SkillsSectionContainer`", () => {
@@ -20,9 +20,10 @@ describe("`SkillsSectionContainer`", () => {
 
     shallow(<SkillsSectionContainer rawSkills={MOCK_RAW_SKILLS} />);
 
-    expect(fetchMock).toHaveBeenCalledWith(GITHUB_REPOS_API_URL, {
-      headers: GITHUB_REPOS_REQUEST_HEADERS
-    });
+    expect(fetchMock).toHaveBeenCalledWith(
+      GITHUB_REPOS_API_URL,
+      GITHUB_REPOS_FETCH_OPTIONS
+    );
     expect(fetchMock).toHaveBeenCalledTimes(1);
   });
 
